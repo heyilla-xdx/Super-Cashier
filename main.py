8652d30a858a8b1d7865545e3fe2040ad25a1137
heyilla-xdx <123818675+heyilla-xdx@users.noreply.github.com>
2023-02-19 21:09:53 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+""" Menu ini digunakan untuk menjalankan aplikasi Super Cashier dengan cara menarik database transaction dan import menu"""
+
+from transaction import menu
+
+if __name__=="__menu__" :
+    menu()
